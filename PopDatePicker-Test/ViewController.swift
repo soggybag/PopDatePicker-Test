6366a0c4fc21909a27fd23e6ba6f74007e1a3890
Mitchell Hudson <soggybag@gmail.com>
2015-12-02 17:22:28 -0800

    init

--- PopDatePicker-Test/ViewController.swift
+++ PopDatePicker-Test/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var popDatePicker: PopDatePicker?
+    
+    
+    // MARK: IBOutlets 
+    
+    // This textField will work with the datepicker.
+    @IBOutlet weak var dateText: UITextField!
+    
+    
+    // MARK: TextField Delegate methods
+    
+    // This method is called when someone taps on the textfield to start entering text.
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        if self.dateText === textField {
+            resign()
+            popDatePicker?.pick(self, initDate: nil, dataChanged: { (newDate: NSDate, forTextField: UITextField) -> () in
+                let formatter = NSDateFormatter()
+                formatter.dateStyle = .MediumStyle
+                forTextField.text = formatter.stringFromDate(newDate)
+            })
+            return false
+        }
+        return true
+    }
+    
+    func resign() {
+        dateText.resignFirstResponder()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        popDatePicker = PopDatePicker(forTextField: dateText)
+        // Set the arrow direction to Left, Right, or Down
+        // popDatePicker?.arrowDirection = .Down
+        dateText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
